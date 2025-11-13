100c227731b527df7cd5b46cba827c358d06fab1
Aitor <aitor.ble@gmail.com>
2025-11-13 14:18:58 +0100

    Refactor image type imports in getBrands function for consistency

--- src/lib/get-brands.ts
+++ src/lib/get-brands.ts
@@ -1,13 +1,13 @@
-import { Images } from '@/interfaces/gallery';
+import { Gallery as GalleryImages } from '@/interfaces/gallery';
 import { query } from './strapi';
 
 export function getBrands() {
   const { STRAPI_HOST } = process.env;
   return query('marcas?populate[imagenes][fields][0]=url').then((res) => {
     const marcas = res.data;
-    const images = marcas[0]?.imagenes ?? ([] as Images[]);
+    const images = marcas[0]?.imagenes ?? ([] as GalleryImages[]);
 
-    const newImages = images.map((img: Images) => ({
+    const newImages = images.map((img: GalleryImages) => ({
       id: img.id,
       url: img.url.startsWith('http') ? img.url : `${STRAPI_HOST}${img.url}`,
     }));
